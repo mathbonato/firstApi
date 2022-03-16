9a9678d13e11fb3fa6d04c0a3b13e806c157df32
Matheus Bonato <matheusmbonato@gmail.com>
2022-03-16 00:19:23 -0300

    adding routes

---
+++ src/config/routes.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from 'express';
+import { ProductController } from '../controllers/ProductController';
+
+const routes = Router();
+
+routes.get("/", (request: Request, response: Response) => {
+    return response.status(201).json({ message: "Deu boa! 2" });
+});
+
+routes.get("/products",  new ProductController().list);
+routes.post("/products",  new ProductController().create);
+routes.put("/product/:id",  new ProductController().update);
+routes.delete("/product/:id",  new ProductController().delete);
+
+
+export { routes };
